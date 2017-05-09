274c097073d77ef806fd42308e29047e9348934b
sharon mcdermott <sharonmcdermott@me.com>
2017-05-09 10:11:11 -0400

    Added an info button to the Storyboard.  Connected the button to the View Controller.  Created a function that would bring up an alert screen with a message and two buttons when you pressed the info button. The alert will dismiss when you press either of the buttons.  To test, the buttons will cause the output to print "YES" or "NO" in the output depending on what button you press.

--- Alert Test/ViewController.swift
+++ Alert Test/ViewController.swift
@@ -14,11 +14,56 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
 
+            let alertController = UIAlertController(title: "Question", message: "Can you dance?", preferredStyle: UIAlertControllerStyle.alert)
+            
+            //creating a button
+            alertController.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.default, handler: { (action) in
+                alertController.dismiss(animated: true, completion: nil)
+                print("YES")
+            }))
+            
+            
+            alertController.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.default, handler: { (action) in
+                alertController.dismiss(animated: true, completion: nil)
+                print("NO")
+            }))
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
+
+
+
+/*
+    func createAlert (title:String, message:String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+
+        //creating a button
+        alert.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+            print("YES")
+        }))
+
+        
+        alert.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+            print("NO")
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+   */
+
+
+/*
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+}
+*/
 
 
 }
